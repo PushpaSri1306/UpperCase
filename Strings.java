2579fbccd70f159e115d21c8a23be93adbdd494f
PushpaSri1306 <puttapushpasri@gmail.com>
2025-01-28 20:29:32 +0530

    Uppercase of a given string

---
+++ Strings.java
@@ -0,0 +1,8 @@
+public class Strings {
+    public static void main(String[] args) {
+        String a = "hello world";
+        String b = a.toUpperCase();
+        System.out.println(b);
+
+    }
+}
